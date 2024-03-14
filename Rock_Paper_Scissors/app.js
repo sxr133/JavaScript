c58e5ade910d6b2ca4119187348b80153b9fdb37
sxr133 <96782879+sxr133@users.noreply.github.com>
2024-03-13 21:03:49 -0400

    added missing semi-colon in my js files

--- Rock_Paper_Scissors/app.js
+++ Rock_Paper_Scissors/app.js
@@ -1,67 +1,67 @@
-const computerChoiceDisplay = document.getElementById('computer-choice')
-const userChoiceDisplay = document.getElementById('user-choice')
-const resultDisplay = document.getElementById('result')
-const possibleChoices = document.querySelectorAll('button')
-let userChoice
-let computeChoice
-let result
+const computerChoiceDisplay = document.getElementById('computer-choice');
+const userChoiceDisplay = document.getElementById('user-choice');
+const resultDisplay = document.getElementById('result');
+const possibleChoices = document.querySelectorAll('button');
+let userChoice;
+let computeChoice;
+let result;
 
 possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click', (e) => {
-  userChoice = e.target.id
-  userChoiceDisplay.innerHTML = userChoice
+  userChoice = e.target.id;
+  userChoiceDisplay.innerHTML = userChoice;
 
-  generateComputerChoice()
+  generateComputerChoice();
 
-  getResult()
+  getResult();
 }))
 
 function generateComputerChoice() {
-  const randomNumber = Math.floor(Math.random() * possibleChoices.length) + 1
+  const randomNumber = Math.floor(Math.random() * possibleChoices.length) + 1;
   
   if (randomNumber === 1) {
-    computerChoice = 'rock'
+    computerChoice = 'rock';
   }
 
   if (randomNumber === 2) {
-    computerChoice = 'scissors'
+    computerChoice = 'scissors';
   }
 
   if (randomNumber === 3) {
-    computerChoice = 'paper'
+    computerChoice = 'paper';
   }
 
-  computerChoiceDisplay.innerHTML = computerChoice
+  computerChoiceDisplay.innerHTML = computerChoice;
 }
 
 function getResult() {
 
   if (computerChoice === userChoice) {
-    result = "it's a draw!"
+    result = "it's a draw!";
   }
 
   if (computerChoice === 'rock' && userChoice === 'paper') {
-    result = "you win!"
+    result = "you win!";
   }
 
   if (computerChoice === 'rock' && userChoice === 'scissors') {
-    result = "you lost!"
+    result = "you lost!";
   }
 
   if (computerChoice === 'paper' && userChoice === 'scissors') {
-    result = "you win!"
+    result = "you win!";
   }  
 
   if (computerChoice === 'paper' && userChoice === 'rock') {
-    result = "you lose!"
+    result = "you lose!";
   }
 
   if (computerChoice === 'scissors' && userChoice === 'paper') {
-    result = "you lose!"
+    result = "you lose!";
   }
 
   if (computerChoice === 'scissors' && userChoice === 'rock') {
-    result = "you win!"
+    result = "you win!";
   }
 
-  resultDisplay.innerHTML = result
+  resultDisplay.innerHTML = result;
 }
